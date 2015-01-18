4524824621fe08458f42cea38e3128f62c5d3ac8
cyjj <cybeater@gmail.com>
2015-01-17 23:02:46 -0600

    Revert "answer"
    This reverts commit 2b6c71efb4823a1c50038598331926ae797634b6.

--- complete.R
+++
@@ -1,8 +0,0 @@
-complete<-function(directory, id =1:332){
-	nobs = numeric(0)
-	for(i in id){
-		data <- read.csv(paste(directory,"/",sprintf("%03d",i),".csv",sep=""))
-		nobs <- c(nobs,sum(!is.na(data$sulfate)& !is.na(data$nitrate)))
-	}
-	data.frame(id=id,nobs=nobs)
-}
